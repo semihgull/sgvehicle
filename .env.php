633e8609e1e22c1afd08d5892dd75df19131138d
Semih Gül <semihx.gull@gmail.com>
2022-01-22 16:53:50 +0300

    Update .env.php
    Change Database Settings

--- .env.php
+++ .env.php
@@ -3,7 +3,7 @@
 return [
     'DB_DRIVER' => 'mysql',
     'DB_HOST' => 'localhost',
-    'DB_NAME' => 'sgboilerplate',
+    'DB_NAME' => 'localhost',
     'DB_USER' => 'root',
     'DB_PASSWORD' => '',
     'DB_CHARSET' => 'utf8mb4',
